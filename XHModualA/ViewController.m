cf452fe3d676410bf868e32bc12504d400913af6
GSmallSea <gxiao_hai@163.com>
2018-11-16 14:37:24 +0800

    first commit

---
+++ XHModualA/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  XHModualA
+//
+//  Created by XH on 2018/11/16.
+//  Copyright © 2018年 XH. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
